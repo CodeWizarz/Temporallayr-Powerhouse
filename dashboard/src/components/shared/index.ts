1c6d23811ef20cd0c0f7868f9001f22a761b7630
bobby <dveloperbobby@gmail.com>
2026-03-09 17:59:00 +0530

    feat: add shared components (ErrorBoundary, LoadingState, PageHeader, etc.)

---
+++ dashboard/src/components/shared/index.ts
@@ -0,0 +1,5 @@
+export { ErrorBoundary } from './ErrorBoundary';
+export { LoadingState, InlineLoader } from './LoadingState';
+export { ErrorBanner } from './ErrorBanner';
+export { PageHeader } from './PageHeader';
+export { ContextSidebar, SidebarNavItem } from './ContextSidebar';
